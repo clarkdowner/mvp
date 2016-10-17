4167eae4ebb1561d3585e3b4aa3ca29ec3a52ccc
clarkdowner <cddowner@gmail.com>
2016-10-17 16:05:02 -0700

    Add filtering display by category

--- app/app.js
+++ app/app.js
@@ -14,19 +14,39 @@ angular.module('groceryShopper', ['ngRoute'])
   // }])
 
   .factory('Items', function() {
-    return [
-      {category: 'Produce', name: 'bananas', carted: false}, 
-      {category: 'Produce',name: 'spinach', carted: false}, 
-      {category: 'Produce',name: 'bread', carted: false},
-      {category: 'Dairy',name: 'milk', carted: false},
-      {category: 'Dairy',name: 'yogurt', carted: false},
-      {category: 'Meats',name: 'eggs', carted: false}
-    ];
+
+    var allItems = function() {
+      return [
+        {category: 'Produce', name: 'bananas', carted: false}, 
+        {category: 'Produce',name: 'spinach', carted: false}, 
+        {category: 'Produce',name: 'bread', carted: false},
+        {category: 'Dairy',name: 'milk', carted: false},
+        {category: 'Dairy',name: 'yogurt', carted: false},
+        {category: 'Meats',name: 'eggs', carted: false}
+      ];
+    };
+
+    var categoryItems = function(category) {
+      var categoryArray = [];
+      var allItems = this.allItems();
+      for (var i = 0; i < allItems.length; i++) {
+        if (allItems[i].category === category) {
+          categoryArray.push(allItems[i]);
+        }
+      }
+      return categoryArray;
+    };
+
+    return {
+      allItems: allItems,
+      categoryItems: categoryItems
+    }
+
   })
 
   .controller('ProduceCtrl', function($scope, Items) {
     $scope.category = 'Produce';
-    $scope.items = Items;
+    $scope.items = Items.categoryItems('Produce');
     $scope.newItem = '';
 
     $scope.addItem = function(itemToAdd) {
@@ -49,7 +69,7 @@ angular.module('groceryShopper', ['ngRoute'])
 
   .controller('DairyCtrl', function($scope, Items) {
     $scope.category = 'Dairy';
-    $scope.items = Items;
+    $scope.items = Items.categoryItems('Dairy');
     $scope.newItem = '';
 
     $scope.addItem = function(itemToAdd) {
